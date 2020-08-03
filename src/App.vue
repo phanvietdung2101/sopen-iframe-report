e75eaefefab0d64448d443f12b446ddea45ec2b6
phanvietdung2101 <phanvietdung2101@gmail.com>
2020-08-03 17:32:01 +0700

    template

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <iframe src="http://192.168.1.9:9528/bierp-embed/R_RP_TEST_2434/s99pfs2vyr703jcm48jjxriaghoichgpjd33ry7dvi4lzr7k53" width="100%"></iframe>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
